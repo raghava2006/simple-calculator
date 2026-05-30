f5e880e0772a80786210ae29aaa7e193a46dd430
raghava2006 <raghavapreetham662@gmail.com>
2026-05-30 13:29:41 +0530

    first Calculator project

---
+++ python_Calculator/calciv1.py
@@ -0,0 +1,7 @@
+print("Welcome to My calculator") 
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+print("Addition =", num1+num2)
+print("Subtraction =", num1-num2)
+print("Multiplication =", num1*num2)
+print("Division =", num1/num2)
